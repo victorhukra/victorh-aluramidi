a0bbb68c2f63da78c5afa7dc8a48f4af54848833
victorhukra <128063057+victorhukra@users.noreply.github.com>
2023-10-19 21:07:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
-function TocaSomPom () {
-    document. querySelector ('#som_tecla_pom').play ();
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector (seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {  
+        elemento.play()
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
-document. querySelector ('.tecla_pom');
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}   
